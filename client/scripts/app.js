984ab954ddf471f6c52567c88af8f3dfc1fef683
mjimaz <m.aljimaz@gmail.com>
2016-01-25 14:57:25 -0800

    create app object with init, send, fetch, clearMessage, addMessage, and addRoom methods

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,48 @@
 // YOUR CODE HERE:
+var app = {};
 
+app.init = function() {};
+
+app.send = function(message) {
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
+
+app.fetch = function() {
+  $.ajax({
+  // This is the url you should use to communicate with the parse API server.
+  type: 'GET',
+  contentType: 'application/json',
+  success: function (data) {
+    console.log('chatterbox: Message received');
+  },
+  error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to receive message');
+  }
+});
+};
+
+app.clearMessages = function() {
+  $('#chats').empty();  
+};
+
+app.addMessage = function(message){
+  $('#chats').append($('<div>', message));
+};
+
+app.addRoom = function(room) {
+  $('#roomSelect').append($('<div>', room));
+};
